e7facdf6b4d4a6e89543a1f525e3f4db61396a4b
iambartek <bartlomiej.kazior@microsoft.wsei.edu.pl>
2022-03-29 21:56:05 +0200

    GetLetterGrade override

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,33 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("There must be at least 5 students");
+            }
+
+            else if (averageGrade >= 80)
+            {
+                return 'A';
+            }
+            else if (averageGrade >= 60)
+            {
+                return 'B';
+            }
+            else if (averageGrade >= 40)
+            {
+                return 'C';
+            }
+            else if (averageGrade >= 20)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+        }
     }
 }
